a49ac17135752d401d612d0c6f88fdbb09691471
Do Xuan Kien <111947701+iKyel@users.noreply.github.com>
2024-03-09 18:27:24 +0700

    first

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var config = {
+  position: 'start',
+  draggable: true,
+  dropOffBoard: 'trash',
+  pieceTheme: function(piece) {
+    // Thay đổi đường dẫn của hình ảnh tương ứng với các quân cờ
+    return './Lib/img/chesspieces/wikipedia/' + piece + '.png';
+  }
+}
+
+var myBoard = Chessboard('myBoard', config);
