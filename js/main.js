d2824a2f566d21272a6e049fcb73ec6eed4a0122
Do Xuan Kien <111947701+iKyel@users.noreply.github.com>
2024-03-10 19:07:18 +0700

    tạo biến cơ bản trong main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,136 @@
+
+var board = null // thông tin bàn cờ
+var $board = $('#myBoard') // tham chiếu đến HTML
+var game = new Chess() // quản lý trạng thái
+var whiteSquareLegal = '#a9a9a9'; // màu cho các ô chứa nước đi legal
+var blackSquareLegal = '#696969'; // màu cho các ô chứa nước đi legal
+var squareClass = 'square-55d63';
+var squareToHighlight = null;
+var colorToHighlight = null;
+var positionCount;
+timer = null;
+
 var config = {
-  position: 'start',
-  draggable: true,
+  position: 'start', // bắt đầu bàn cờ
+  draggable: true,   // cho phép kéo thả quân cờ
   dropOffBoard: 'trash',
-  pieceTheme: function(piece) {
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onMouseoutSquare: onMouseoutSquare,
+  onMouseoverSquare: onMouseoverSquare,
+  onSnapbackEnd: onSnapbackEnd,
+  pieceTheme: function (piece) {
     // Thay đổi đường dẫn của hình ảnh tương ứng với các quân cờ
     return './Lib/img/chesspieces/wikipedia/' + piece + '.png';
   },
-  
 }
 
 var myBoard = Chessboard('myBoard', config);
+
+/*
+  Khai báo bảng giá trị cho từng loại quân cờ và giá trị vị trí trên bàn cờ, nguồn:
+  https://www.chessprogramming.org/Simplified_Evaluation_Function#Pawns
+*/
+
+// P: Pawn (Tốt)
+// N: Knight (Mã)
+// B: Bishop (Tượng)
+// R: Rook (Xe)
+// Q: Queen (Hậu)
+// K: King (Tướng)
+// K_E: King End (Tướng khi hết cờ)
+var values = { p: 100, n: 320, b: 330, r: 500, q: 900, k: 20000, k_e: 20000 };
+
+// pst = piece square tables
+// giá trị của quân trắng
+var pst_w = {
+  p: [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [50, 50, 50, 50, 50, 50, 50, 50],
+    [10, 10, 20, 30, 30, 20, 10, 10],
+    [5, 5, 10, 25, 25, 10, 5, 5],
+    [0, 0, 0, 20, 20, 0, 0, 0],
+    [5, -5, -10, 0, 0, -10, -5, 5],
+    [5, 10, 10, -20, -20, 10, 10, 5],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+  ],
+  n: [
+    [-50, -40, -30, -30, -30, -30, -40, -50],
+    [-40, -20, 0, 0, 0, 0, -20, -40],
+    [-30, 0, 10, 15, 15, 10, 0, -30],
+    [-30, 5, 15, 20, 20, 15, 5, -30],
+    [-30, 0, 15, 20, 20, 15, 0, -30],
+    [-30, 5, 10, 15, 15, 10, 5, -30],
+    [-40, -20, 0, 5, 5, 0, -20, -40],
+    [-50, -40, -30, -30, -30, -30, -40, -50]
+  ],
+  b: [
+    [-20, -10, -10, -10, -10, -10, -10, -20],
+    [-10, 0, 0, 0, 0, 0, 0, -10],
+    [-10, 0, 5, 10, 10, 5, 0, -10],
+    [-10, 5, 5, 10, 10, 5, 5, -10],
+    [-10, 0, 10, 10, 10, 10, 0, -10],
+    [-10, 10, 10, 10, 10, 10, 10, -10],
+    [-10, 5, 0, 0, 0, 0, 5, -10],
+    [-20, -10, -10, -10, -10, -10, -10, -20]
+  ],
+  r: [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [5, 10, 10, 10, 10, 10, 10, 5],
+    [-5, 0, 0, 0, 0, 0, 0, -5],
+    [-5, 0, 0, 0, 0, 0, 0, -5],
+    [-5, 0, 0, 0, 0, 0, 0, -5],
+    [-5, 0, 0, 0, 0, 0, 0, -5],
+    [-5, 0, 0, 0, 0, 0, 0, -5],
+    [0, 0, 0, 5, 5, 0, 0, 0]
+  ],
+  q: [
+    [-20, -10, -10, -5, -5, -10, -10, -20],
+    [-10, 0, 0, 0, 0, 0, 0, -10],
+    [-10, 0, 5, 5, 5, 5, 0, -10],
+    [-5, 0, 5, 5, 5, 5, 0, -5],
+    [0, 0, 5, 5, 5, 5, 0, -5],
+    [-10, 5, 5, 5, 5, 5, 0, -10],
+    [-10, 0, 5, 0, 0, 0, 0, -10],
+    [-20, -10, -10, -5, -5, -10, -10, -20]
+  ],
+  k: [
+    [-30, -40, -40, -50, -50, -40, -40, -30],
+    [-30, -40, -40, -50, -50, -40, -40, -30],
+    [-30, -40, -40, -50, -50, -40, -40, -30],
+    [-30, -40, -40, -50, -50, -40, -40, -30],
+    [-20, -30, -30, -40, -40, -30, -30, -20],
+    [-10, -20, -20, -20, -20, -20, -20, -10],
+    [20, 20, 0, 0, 0, 0, 20, 20],
+    [20, 30, 10, 0, 0, 10, 30, 20]
+  ],
+
+  // Endgame King Table
+  k_e: [
+    [-50, -40, -30, -20, -20, -30, -40, -50],
+    [-30, -20, -10, 0, 0, -10, -20, -30],
+    [-30, -10, 20, 30, 30, 20, -10, -30],
+    [-30, -10, 30, 40, 40, 30, -10, -30],
+    [-30, -10, 30, 40, 40, 30, -10, -30],
+    [-30, -10, 20, 30, 30, 20, -10, -30],
+    [-30, -30, 0, 0, 0, 0, -30, -30],
+    [-50, -30, -30, -30, -30, -30, -30, -50]
+  ],
+};
+
+// giá trị của quân đen
+var pst_b = {
+  p: pst_w['p'].slice().reverse(),
+  n: pst_w['n'].slice().reverse(),
+  b: pst_w['b'].slice().reverse(),
+  r: pst_w['r'].slice().reverse(),
+  q: pst_w['q'].slice().reverse(),
+  k: pst_w['k'].slice().reverse(),
+  k_e: pst_w['k_e'].slice().reverse(),
+};
+
+// 
+var pstEnemy = { w: pst_b, b: pst_w };
+var pstMine = { w: pst_w, b: pst_b };
 
 
